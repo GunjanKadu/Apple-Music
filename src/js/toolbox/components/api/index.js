99a124a67ee256f49bde4a7172b0f27a44ac49ce
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-04 23:52:19 +0100

    Adding Tool Box api component

---
+++ src/js/toolbox/components/api/index.js
@@ -0,0 +1,61 @@
+class ApiClass {
+  constructor() {
+    this.url = `https://57c62f9e.ngrok.io/api`;
+  }
+
+  makeRequest = ({ url, data }) => {
+    return new Promise(function(resolve, reject) {
+      fetch(url, data)
+        .then(response => {
+          response.json().then(data => {
+            if (response.status >= 300) {
+              reject(data.message);
+            }
+            resolve(data);
+          });
+        })
+        .catch(e => {
+          reject(Error(e));
+        });
+    });
+  };
+
+  fetchArtists = () => {
+    return this.makeRequest({
+      url: `${this.url}/artists`,
+      data: {
+        method: "GET"
+      }
+    });
+  };
+
+  fetchArtist = artist => {
+    return this.makeRequest({
+      url: `${this.url}/artist/${artist}`,
+      data: {
+        method: "GET"
+      }
+    });
+  };
+
+  fetchAlbums = () => {
+    return this.makeRequest({
+      url: `${this.url}/albums`,
+      data: {
+        method: "GET"
+      }
+    });
+  };
+
+  fetchAlbum = ({ album }) => {
+    return this.makeRequest({
+      url: `${this.url}/album/${album}`,
+      data: {
+        method: "GET"
+      }
+    });
+  };
+}
+
+const Api = new ApiClass();
+export default Api;
