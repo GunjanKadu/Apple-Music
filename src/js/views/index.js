be7b414bca15af61eac791b647ec17c0d3f3354a
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-04 23:46:52 +0100

    Implementing View Container

---
+++ src/js/views/index.js
@@ -0,0 +1,7 @@
+export { default as Library } from './library';
+export { default as Artists } from './artist_list';
+export { default as Artist } from './artist';
+export { default as Albums } from './album_list';
+export { default as Album } from './album';
+export { default as Playlists } from './playlist_list';
+export { default as Playlist } from './playlist';
